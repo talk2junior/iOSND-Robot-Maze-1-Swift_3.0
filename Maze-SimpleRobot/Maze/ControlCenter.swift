0986868d0baf645011525c78abb425be65d04607
talk2junior <talk2junior@gmail.com>
2017-01-05 14:17:24 -0500

    Initial upload for iOSND course
    iOSND robot maze 1 files. Use for exercises and practice via Udacity
    iOSND program.

---
+++ Maze-SimpleRobot/Maze/ControlCenter.swift
@@ -0,0 +1,25 @@
+//
+//  ControlCenter.swift
+//  Maze
+//
+//  Created by Jarrod Parkes on 8/14/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+
+class ControlCenter {
+    
+    func moveSimpleRobot(robot: SimpleRobot) {
+    
+        robot.moveLeft()
+        robot.moveUp()
+        robot.moveLeft()
+        robot.moveDown()
+        robot.moveDown()
+        robot.moveDown()
+        robot.moveLeft()
+        robot.moveUp()
+        robot.moveUp()
+        robot.moveUp()
+        
+    }
+}
